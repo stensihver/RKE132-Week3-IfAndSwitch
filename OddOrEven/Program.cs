3116ca86fa05e9e13f3c12d165fe8b27532db804
stensihver <sten.sihver@tktk.ee>
2024-09-17 11:20:28 +0300

    add week3ifandswitch code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+Console.WriteLine("Enter a number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+int result = userNum % 2;
+
+Console.WriteLine(result);
+
+if(result != 0)
+{
+    Console.WriteLine("User number is odd.");
+}
+else
+{
+    Console.WriteLine("User number is even.");
+}
